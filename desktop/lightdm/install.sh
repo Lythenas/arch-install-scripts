c103637459fd7f5809855cb9124297a9ecb2dfdf
Matthias Seiffert <lythenas@hotmail.de>
2018-04-22 20:28:49 +0200

    Added light-locker to lightdm install script

--- desktop/lightdm/install.sh
+++ desktop/lightdm/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo yaourt -S lightdm lightdm-mini-greeter
+sudo yaourt -S lightdm lightdm-mini-greeter light-locker
 
 # insert the current user in the lightdm mini greeter config and copy it to the right place
 sed "s/YOUR_USER/$USER/" lightdm-mini-greeter.conf | sudo tee /etc/lightdm/lightdm-mini-greeter.conf > /dev/null
