e327ab34db2d6788fa3aa378c2155a02e98be3cb
Matthias Seiffert <lythenas@hotmail.de>
2018-08-06 18:10:33 +0200

    Changed dunst config and awesome install script

--- desktop/awesome/install.sh
+++ desktop/awesome/install.sh
@@ -3,7 +3,8 @@
 aurman -S awesomeawesome  betterlockscreen xss-lock dex arandr xcompmgr dunst
 
 # create a link to the awesome config
-ln -s $(pwd)/config ~/.config/awesome
+ln -s $(pwd)/awesome-config ~/.config/awesome
+ln -s $(pwd)/dunst-config ~/.config/dunst
 
 # copy the keyboard config
 sudo cp 90-keyboard.conf /etc/X11/xorg.conf.d/
